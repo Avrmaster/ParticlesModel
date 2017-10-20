86658e4dcd8263481c541696cc655701cfa720e4
Oleksandr Leskiv <sasha26.05.2012@gmail.com>
2017-10-21 00:41:12 +0300

    Initial 3D

---
+++ core/src/ua/leskivproduction/particlesmodel/Model/Particle3D.java
@@ -0,0 +1,9 @@
+package ua.leskivproduction.particlesmodel.Model;
+
+public class Particle3D extends Particle2D {
+
+    public Particle3D(int partN, int PARTICLES_COUNT, int MAX_X, int MAX_Y) {
+        super(partN, PARTICLES_COUNT, MAX_X, MAX_Y);
+    }
+
+}
